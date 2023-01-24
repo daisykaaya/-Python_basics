e27f0c697b19079e4c6d7bf5d43f8d6c553157c0
daisykaaya <daisykaaya@gmail.com>
2023-01-24 16:28:16 +0300

    added the check_win function

--- learn/learn.py
+++ learn/learn.py
@@ -5,5 +5,23 @@ def get_choices():
     computer_choice = random.choice(options)
     choices = {"player": player_choice, "computer":computer_choice}
     return choices
+
+def check_win(player, computer):
+    print(f"You chose  {player} Computer chose  {computer}")
+    if player == computer:
+        return "it's a tie"
+    elif player == "rock":
+        if computer == "scissors":
+            return "Rocks smarshes scissors. You Win!"
+        else: 
+            return "Papers cover rocks! You lose!"
+    elif player == "paper":
+        if computer == "rock":
+            return "Paper covers rock! You win!"
+        else: "Scissors cut paper! You lose!"
+    elif player == "scissors":
+        if computer == "paper":
+            return "Scissors cut paper! You win!"
+        else: 
+            return "Rock smarshes scissors! You lose!"
 choices = get_choices()
-print(choices) 
